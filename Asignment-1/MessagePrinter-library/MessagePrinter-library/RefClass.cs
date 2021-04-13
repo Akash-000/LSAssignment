7e597826b97457dbefde750801ff9cdb160386aa
Akash-000 <straw.hat.pirates2001@gmail.com>
2021-04-13 11:30:10 +0530

    FINALIZED

---
+++ Asignment-1/MessagePrinter-library/MessagePrinter-library/RefClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessagePrinter_library
+{
+    public class RefClass : Exception
+    {
+        public string printer(string message)           //method to print the message
+        {
+            if (message == "")                   //Check for empty message
+                throw new ArgumentException("Enter valid string"); 
+            else
+            {
+                return "Hello " + message;
+            }
+        }
+    }
+}
